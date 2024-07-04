2968e57e67e316abf9eacfa8586f191d56b7d703
Fernando Carminatti <nandocarminatti@hotmail.com>
2024-07-04 13:53:39 -0300

    Starting point

---
+++ src/test/java/com/example/Associates/AssociatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Associates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
