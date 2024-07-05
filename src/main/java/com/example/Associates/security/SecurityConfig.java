faae2a26782a22f2f73f3010f56e111a68fc7bd0
Fernando Carminatti <nandocarminatti@hotmail.com>
2024-07-05 16:01:13 -0300

    Change: Register behavior to only admin roles | Need further Adjustments

--- src/main/java/com/example/Associates/security/SecurityConfig.java
+++ src/main/java/com/example/Associates/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
